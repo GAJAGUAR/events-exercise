0994034531bbcda38531111c129b619fbe29190a
G.A.JAGUAR <gajaguar.git@gmail.com>
2018-05-28 20:57:42 -0500

    Ready canvas and control panel
    Implemented a control panel that allows to choose color, line width, line join and line cap. Presents a bug that creates a point by pressing the "Clear" button.

--- keyboard.js
+++ keyboard.js
@@ -1,8 +1,7 @@
 var canvasId = "myCanvas";
-var element = document.getElementById(canvasId);
-var color = "brown";
-var width = 5;
-var join = "miter";
+var color = "#ffffff";
+var width = 1;
+var join = "round";
 var cap = "round";
 var xStart;
 var yStart;
@@ -10,6 +9,18 @@ var xFinal;
 var yFinal;
 var x;
 var y;
+
+var element = document.getElementById(canvasId);
+var colorText = document.getElementById("txtColor");
+var lineWidthText = document.getElementById("txtLineWidth");
+var bevelRadio = document.getElementById("radBevel");
+var roundRadioA = document.getElementById("radRoundA");
+var miterRadio = document.getElementById("radMiter");
+var buttRadio = document.getElementById("radButt");
+var roundRadioB = document.getElementById("radRoundB");
+var squareRadio = document.getElementById("radSquare");
+var applyButton = document.getElementById("btnApply");
+var clearButton = document.getElementById("btnClear");
 
 function drawLine(canvasId, color, width, join, cap, xStart, yStart, xFinal, yFinal) {
     var canvas = document.getElementById(canvasId);
@@ -40,8 +51,10 @@ function pencilDown() {
     xStart = x;
     yStart = y;
     element.addEventListener("mousemove", gettingCoords);
-    element.addEventListener("mousemove", function() { xFinal = x;
-        yFinal = y; });
+    element.addEventListener("mousemove", function() {
+        xFinal = x;
+        yFinal = y;
+    });
     element.addEventListener("mousemove", draw);
 }
 
@@ -49,6 +62,35 @@ function pencilUp() {
     element.removeEventListener("mousemove", gettingCoords);
 }
 
+function clear() {
+    var canvas = document.getElementById(canvasId);
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    context.clearRect(0, 0, w, h);
+}
+
+function apply() {
+    color = colorText.value;
+    width = lineWidthText.value;
+    if (bevelRadio.checked == true) {
+        join = "bevel";
+    } else if (roundRadioA.checked == true) {
+        join = "round";
+    } else {
+        join = "miter";
+    }
+    if (buttRadio.checked == true) {
+        cap = "butt";
+    } else if (roundRadioB.checked == true) {
+        cap = "round";
+    } else {
+        cap = "square";
+    }
+}
+
+clearButton.addEventListener("click", clear);
 element.addEventListener("mousedown", gettingCoords);
+element.addEventListener("mousedown", apply);
 element.addEventListener("mousedown", pencilDown);
 element.addEventListener("mouseup", pencilUp);
